add621cf8305c43901ff7730b763caa01ff7e1a9
Josafat-Mattias Burmeister <josafat-mattias.burm@hpi.de>
2025-03-05 12:49:24 +0100

    fix PointConv import

--- fsct/model.py
+++ fsct/model.py
@@ -4,7 +4,7 @@ from torch_geometric.nn import knn_interpolate
 from torch_geometric.utils import intersection_and_union as i_and_u
 from torch.nn import Sequential as Seq, Linear as Lin, ReLU, BatchNorm1d as BN
 from torch_geometric.data import Dataset, DataLoader, Data
-from torch_geometric.nn import PointConv, fps, radius, global_max_pool
+from torch_geometric.nn import PointNetConv, fps, radius, global_max_pool
 import numpy as np
 import glob
 import pandas as pd
@@ -27,7 +27,7 @@ class SAModule(torch.nn.Module):
         super(SAModule, self).__init__()
         self.ratio = ratio
         self.r = r
-        self.conv = PointConv(NN)
+        self.conv = PointNetConv(NN)
 
     def forward(self, x, pos, batch):
         idx = fps(pos, batch, ratio=self.ratio)
